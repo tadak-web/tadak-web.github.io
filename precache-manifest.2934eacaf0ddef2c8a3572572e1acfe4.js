7321433156e9514fb7acf193ab8c25a0e7e0e71f
josw123 <nonswing.z@gmail.com>
2020-12-09 00:19:28 +0900

    first commit

---
+++ precache-manifest.2934eacaf0ddef2c8a3572572e1acfe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8d774f7ce6df75d76e75e8452cba47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daedb4cd9ab2c2cf79cf",
+    "url": "/static/js/2.16631599.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.16631599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b296825cdad562c55b6d",
+    "url": "/static/js/main.1d177ce1.chunk.js"
+  },
+  {
+    "revision": "85e7246fc0e24cbc46f4",
+    "url": "/static/js/runtime-main.d7c75d3d.js"
+  }
+]);
