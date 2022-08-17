d71885726b2a6904e30ebdb3c3f6819d922ed12b
Sungwoo Jo <nonswing.z@gmail.com>
2022-08-17 14:52:05 +0900

    Updates

---
+++ precache-manifest.7b09c3c0492f50b09717e5f923d2ed25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e84d9e7e4b87250c2fc5ec1c938209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a44fe0b5d839126c9053",
+    "url": "/static/js/2.4fd06e66.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/static/js/2.4fd06e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ff38ee584429e8f9bb",
+    "url": "/static/js/main.c749c487.chunk.js"
+  },
+  {
+    "revision": "85e7246fc0e24cbc46f4",
+    "url": "/static/js/runtime-main.d7c75d3d.js"
+  }
+]);
